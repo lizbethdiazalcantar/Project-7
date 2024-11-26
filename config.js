777a411a5a8c84359baccc8f454d42adfbf5405f
Lizbeth Diaz Alcantar <lizbethdiazalcantar@gmail.com>
2024-11-26 08:18:36 -0500

    Updated server link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: " https://cnt-08c6c54a-05aa-4447-b1b3-56012a54ed4b.containerhub.tripleten-services.com"
+    API_URL: " https://cnt-86cb17cd-7051-47ea-bcfb-5f84212d4156.containerhub.tripleten-services.com"
 }
